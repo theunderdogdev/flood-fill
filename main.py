7db149af87dc7507a1004c557f35552945822921
Ruturaj <ruturaj2404@gmail.com>
2021-12-06 20:44:25 +0530

    Resolved print_matrix bugs

--- main.py
+++ main.py
@@ -10,18 +10,23 @@ def colored(rd, gn, bl, text):
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(rd, gn, bl, text)
 
 
-def print_mat(matrix):
-    print(" ", *[i for i in range(0, 10)], sep="   ", end="  ")
-    print(*[i for i in range(10, 20)], sep="  ")
-    for i, row in enumerate(matrix):
-        if i <= 9:
-            print(i, end="   ")
-        else:
-            print(i, end="  ")
-        for col in row:
-            print(col, end="  ")
+def print_mat(matrix, columns):
+    if columns < 9:
+        print(" ", *[j for j in range(0, columns)], sep="   ", end="  ")
+    else:
+        print(" ", *[j for j in range(0, 10)], sep="   ", end="  ")
+    if columns > 10:
+        print(*[j for j in range(10, columns)], sep="  ")
+    else:
         print()
-
+    for k, rows in enumerate(matrix):
+        if k <= 9:
+            print(k, end="   ")
+        else:
+            print(k, end="  ")
+        for cols in rows:
+            print(cols, end="  ")
+        print()
 # check if it is possible to go to pixel (x, y) from the
 # current pixel. The function returns false if the pixel
 # has a different color, or it's not a valid pixel
@@ -69,21 +74,25 @@ def flood_fill(mat, x, y, replacement):
 
 if __name__ == '__main__':
     # matrix showing portion of the screen having different colors
-
+    rs = int(input("Enter Number of rows: "))
+    cs = int(input("Enter Number of cols: "))
     colors = [colored(0, 255, 0, 'G'), colored(255, 0, 0, 'R'), colored(255, 255, 0, 'Y'), colored(153, 51, 255, 'P')]
     matrice = []
     v = None
-    for _ in range(20):
+    for _ in range(rs):
         temp = []
-        for i in range(20):
+        for i in range(cs):
             v = random.randint(0, len(colors) - 1)
             temp.append(colors[v])
         matrice.append(temp)
-    print_mat(matrice)
-    r = int(input("Enter row number: "))
-    c = int(input("Enter col number: "))
-    replaced = colored(153,255,255,'C')
-    flood_fill(matrice, r, c, replaced)
-    print_mat(matrice)
+    yes = 'y'
+    while yes == 'y':
+        print_mat(matrice,cs)
+        r = int(input("Enter row number: "))
+        c = int(input("Enter col number: "))
+        replaced = colored(153,255,255,'C')
+        flood_fill(matrice, r, c, replaced)
+        print_mat(matrice,cs)
+        yes = input("Do you want to flood fill the matrix again (y/n):").lower()
     # print the colors after replacement
 
--- temp.py
+++ temp.py
@@ -5,9 +5,15 @@ def colored(r, g, b, text):
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
 
 
-def print_mat(matrix):
-    print(" ", *[i for i in range(0, 10)], sep="   ", end="  ")
-    print(*[i for i in range(10, 20)], sep="  ")
+def print_mat(matrix,rows, columns):
+    if columns < 9:
+        print(" ", *[i for i in range(0, columns)], sep="   ", end="  ")
+    else:
+        print(" ", *[i for i in range(0, 10)], sep="   ", end="  ")
+    if columns > 10:
+        print(*[i for i in range(10, columns)], sep="  ")
+    else:
+        print()
     for i, row in enumerate(matrix):
         if i <= 9:
             print(i, end="   ")
@@ -17,17 +23,18 @@ def print_mat(matrix):
             print(col, end="  ")
         print()
 
+
 colors = [colored(0, 255, 0, 'G'), colored(255, 0, 0, 'R'), colored(255, 255, 0, 'Y'), colored(153, 51, 255, 'P')]
 mat = []
 v = 'R'
 
 for _ in range(20):
     temp = []
-    for i in range(20):
+    for i in range(5):
         v = random.randint(0, len(colors) - 1)
         temp.append(colors[v])
     mat.append(temp)
 
 
-
-print_mat(mat)
+for _ in range(3):
+    print_mat(mat, 20,5)
